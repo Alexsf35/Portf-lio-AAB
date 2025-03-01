0a5b0801567c22fc8905405893d85c0f9a23f1e5
B-Neil <148569027+B-Neil@users.noreply.github.com>
2025-03-01 17:03:22 +0000

    gibbs.py updated

--- Motifs/Gibbs sampling.py
+++ Motifs/Gibbs sampling.py
@@ -2,7 +2,8 @@ import random
 
 #1) Escolher posições iniciais de forma aleatória s = (s1,...,st) e formar os segmentos respectivos. 
 
-seqs = ["ATGGTCGC","ATGTCTGA","CCGTAGTA","ATGCATGCATCGATC"]
+seqs = ["GTAAACAATATTTATAGC","AAAATTTACCTCGCAAGG","CCGTACTGTCAAGCGTGG","TGAGTAAACGACGTCCCA","TACTTAACACCCTGTCAA"]
+tam_motif = 8
 def pos_init(seqs: list,tam_motif: int):
     tam_seqs = []
     for i in seqs:
@@ -14,9 +15,8 @@ def pos_init(seqs: list,tam_motif: int):
         pos.append(ni)
     
     return pos
-pos_i= pos_init(seqs,4)
+pos_i= pos_init(seqs,tam_motif)
 print(pos_i)
-
 
 #2) Escolher aleatoriamente uma sequência i
 
@@ -35,7 +35,7 @@ def choose_seq(seqs,pos_i, tam_motif):
     motif[1].pop(index)
     return random_seq, seqs2,motif
 
-chosen_seq, seqs2, motifs = choose_seq(seqs,pos_i,4)
+chosen_seq, seqs2, motifs = choose_seq(seqs,pos_i,tam_motif)
 print(chosen_seq)
 print(seqs2)
 print(motifs)
@@ -59,7 +59,7 @@ def matriz_oc(seqs: list, pseudocont: float = 0):
     
     return mat
 
-ocorrencias = matriz_oc(motifs[1])
+ocorrencias = matriz_oc(motifs[1],pseudocont=0.5)
 print(ocorrencias)
 
 
@@ -112,18 +112,43 @@ for nuc, values in pwm_matrix.items():
 seq_consensos = consenso(pwm_matrix)
 print(seq_consensos)
 
-
 #4) Para cada posição p na sequência i, calcular a probabilidade do segmento iniciado em p com tamanho L, ser gerado por P. 
 
 def prob_p(chosen_seq,tam_motif,pwm_matrix):
     #Creating the motifs
     motifs_i = []
-    for i in range(0,len(chosen_seq)-tam_motif+1):
+    for i in range(len(chosen_seq)-tam_motif+1):
         motifs_i.append(chosen_seq[i:i+tam_motif])
     
     #Calculating probabilities
-    
+    motif_probs={}
+    for motif in motifs_i:
+        probs = 1
 
-prob_p(chosen_seq,4,pwm_matrix)
+        for k,nuc in enumerate(motif):
+            probs *= pwm_matrix[nuc][k]
+
+        motif_probs[motif] = probs
+
+    return motif_probs
+
+
+results = prob_p(chosen_seq,tam_motif,pwm_matrix)
+print(results)
+
+#5) Escolher p de modo de forma estocástica, de acordo com as probabilidades calculadas em 4). 
+
+def normalize_probabilities(motif_probs):
+    normalized_probs = {}
+    sum = 0
+    for value in motif_probs.values():
+        sum += value
+    print(sum)
+    for (motif, prob) in motif_probs.items():
+        normalized_probs[motif] = (prob/sum)
+
+    return normalized_probs
+normalize_probabilities(results)
+
 
 
